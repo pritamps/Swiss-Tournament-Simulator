a2930616b30dfe4691f2f70610c4997783ff58d4
Pritam Sukumar <pritamps@gmail.com>
2017-03-13 18:03:35 +0530

    Full working. Yay!

--- tournament.py
+++ tournament.py
@@ -4,34 +4,104 @@
 #
 
 import psycopg2
+import bleach
+import math
+
+DB_NAME = "tournaments"
+TOURNAMENT_NAME = "Wimbledon"
+TOURNAMENT_YEAR = "2017"
+TOURNAMENT_SPORT = "TENNIS"
+
+
+def createTournament(tournament_name=TOURNAMENT_NAME,
+                     year=TOURNAMENT_YEAR, sport=TOURNAMENT_SPORT):
+    """Creates a new tournament and returns its ID"""
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    # Safe insert into DB. USE QUERY PARAMETERS INSTEAD OF STRING SUBSTITUTION
+    c.execute("INSERT INTO tournaments (name, year, sport ) VALUES (%s, %s, %s) RETURNING id", 
+                    (bleach.clean(tournament_name), bleach.clean(year), bleach.clean(sport), ))
+    tournament_id = c.fetchone()
+    DB.commit()
+    DB.close()
+    return tournament_id
+
+
+def getTournamentID(tournament_name=TOURNAMENT_NAME,
+                    year=TOURNAMENT_YEAR, sport=TOURNAMENT_SPORT):
+
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    c.execute("SELECT id from tournaments WHERE name='" + tournament_name + "' AND year='" + year + "'")
+    rows = c.fetchone()
+    if rows is None:
+        return None
+    else:
+        return rows[0]
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    return psycopg2.connect("dbname=" + DB_NAME)
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    c.execute("DELETE from matches")
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    c.execute("DELETE from matches")
+    c.execute("DELETE from players")
+    DB.commit()
+    DB.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    c.execute("SELECT count(*) FROM players")
+    count = c.fetchone()[0]
+    DB.close()
+
+    return count
 
 
-def registerPlayer(name):
+def registerPlayer(name, tournament_name=TOURNAMENT_NAME,
+                   year=TOURNAMENT_YEAR):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+
+    # Get tournament ID for given tournament. If it doesn't exist, create it
+    c.execute("SELECT id from tournaments WHERE name='" + tournament_name + "' AND year='" + year + "'")
+    rows = c.fetchone()
+    if rows is None:
+        tournament_id = createTournament()[0]
+    else:
+        tournament_id = rows[0]
+
+    # Add new player (if they don't exist already) and get their ID
+    c.execute("INSERT into players (name) VALUES (%s) RETURNING id", (bleach.clean(name),))
+    player_id = c.fetchone()[0]
+
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
@@ -47,25 +117,54 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    list_of_tuples = []
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    c.execute("SELECT * FROM player_standings")
+    rows = c.fetchall()
+    tournament_id = getTournamentID(tournament_name=TOURNAMENT_NAME,
+                                    year=TOURNAMENT_YEAR,
+                                    sport=TOURNAMENT_SPORT)
+    for row in rows:
+        list_of_tuples.append(row)
+    DB.close()
+    return list_of_tuples
 
 
-def reportMatch(winner, loser):
+def reportMatch(winner, loser,
+                tournament_name=TOURNAMENT_NAME, year=TOURNAMENT_YEAR, round=-1):
     """Records the outcome of a single match between two players.
 
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+
+    # Get tournament ID for given tournament. If it doesn't exist, create it
+    c.execute("SELECT id from tournaments WHERE name='" + tournament_name + "' AND year='" + year + "'")
+    rows = c.fetchone()
+
+    if rows is None:
+        tournament_id = createTournament()[0]
+    else:
+        tournament_id = rows[0]
+
+    c.execute("INSERT INTO matches (winner, loser, tournament_id, round) VALUES (%s, %s, %s, %s)", (winner, loser, tournament_id, round) )
+
+    DB.commit()
+    DB.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +172,18 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+
+    DB = psycopg2.connect("dbname=" + DB_NAME)
+    c = DB.cursor()
+    standings = playerStandings()
+
+    # Start with round 1
+    round = 1
+    list_of_tuples = []
+    for i in range(0, len(standings), 2):
+        list_of_tuples.append((standings[i][0], standings[i][1], standings[i+1][0], standings[i+1][1]))
+
+    return list_of_tuples
+
 
 
--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,45 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+
+-- Connect to the tournaments database. 
+
+DROP DATABASE IF EXISTS tournaments;
+CREATE DATABASE tournaments;
+\c tournaments;
+
+-- Create players database
+CREATE TABLE players ( id SERIAL PRIMARY KEY,
+                       name TEXT );
+
+-- Table of all tournaments. 
+CREATE TABLE tournaments ( id SERIAL PRIMARY KEY,
+                           name TEXT,
+                           sport TEXT,
+                           year TEXT, 
+                           UNIQUE ( name, year ) );
+
+-- Create matches database
+CREATE TABLE matches ( id SERIAL PRIMARY KEY,
+                      winner SERIAL references players(id),
+                      loser SERIAL references players(id),
+                      tournament_id SERIAL references tournaments(id),
+                      round SMALLINT );
+
+-- Create view for wins and losses for players
+CREATE VIEW player_wins AS 
+                    ( SELECT players.name as name, players.id as id, count(matches.winner) AS wins 
+                      FROM players LEFT JOIN matches ON players.id=matches.winner 
+                      GROUP by players.id );
+
+CREATE VIEW player_matches AS 
+                    ( SELECT players.name as name, players.id as id, count(matches.winner) AS matches 
+                      FROM players LEFT JOIN matches ON ( players.id=matches.winner OR players.id=matches.loser )
+                      GROUP by players.id );
+
+CREATE VIEW player_standings AS 
+                    ( SELECT player_wins.id, player_wins.name, wins, matches 
+                      FROM (player_wins INNER JOIN player_matches ON player_wins.id=player_matches.id) 
+                      ORDER BY wins DESC);
+
+
 
 
